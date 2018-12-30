b60aa4bb35fca2b6117d1a43e6c672901b74f018
Dinesh Reddy <dineshreddy@Dineshs-MacBook-Pro.local>
2018-12-30 17:12:10 -0500

    initial commit

---
+++ src/main/java/com/dinesh/demomavenproject/DemoMavenprojectApplication.java
@@ -0,0 +1,14 @@
+package com.dinesh.demomavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenprojectApplication.class, args);
+	}
+
+}
+
